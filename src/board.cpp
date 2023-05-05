4f46bc1e205fdebc6d653dd0bef99b93ce6c6d6c
溝田　正 <104009590+amichi5963@users.noreply.github.com>
2023-05-05 16:33:39 +0900

    Revert "転写"
    This reverts commit c553f23e09485b3c31a3692cf03ad65c637d82a3.

--- src/board.cpp
+++ src/board.cpp
@@ -1,50 +1,21 @@
 #include "board.h"
 
-bool Board::find(const Point& start, const Point& goal) {
-	{
-		Mass& mass_start = getMass(start);
-		Mass& mass_goal = getMass(goal);
+bool Board::find(const Point& start, const Point& goal)
+{
+	mass_[start.y()][start.x()].setStatus(Mass::START);
+	mass_[goal.y()][goal.x()].setStatus(Mass::GOAL);
 
-		mass_start.setStatus(Mass::START);
-		mass_goal.setStatus(Mass::GOAL);
+	Point p = start;
+	while (p != goal) {
+		if (p != start) mass_[p.x()][p.y()].setStatus(Mass::WAYPOINT);
 
-		//オープンリストに開始ノードを追加する
-		open_list_.clear();
-		open_list_.push_back(&mass_start);
-
-		while (!open_list_.empty()) {//オープンリストが空でない
-			//現在のノード=オープンリストの最も安価なリスト
-			std::sort(open_list_.begin(), open_list_.end(), asc);
-			auto it = open_list_.begin();
-			Mass* current = *it;
-			if (current->getStatus() == Mass::GOAL) {//目的地なら経路の完成
-				Mass* p = current;//経路のステータスをMass::WAYPOINTにする
-				while (p) { if (p->getStatus() == Mass::BLANK) p->setStatus(Mass::WAYPOINT); p = p->getParent(); }
-				return true;
-			}
-			else {
-				//現在のノードをクローズドリストに移す
-				open_list_.erase(it);
-				current->setListed(Mass::CLOSE);
-				//現在のノードの隣接する各ノードを調べる
-				const Point& pos = current->getPos();
-				Point next[4] = { pos.getRight(),pos.getLeft(),pos.getUp(),pos.getDown() };
-				for (auto& c : next) {//隣接ノード
-					if (c.x() < 0 || BOARD_SIZE <= c.x()) continue;//マップ外ならスキップ
-					if (c.y() < 0 || BOARD_SIZE <= c.y()) continue;
-					Mass& m = getMass(c);
-					if (!m.isListed(Mass::OPEN) &&//オープンリストに含まれていない
-						!m.isListed(Mass::CLOSE) &&//クローズドリストに含まれていない
-						m.getStatus() != Mass::WALL) {//障害物でない
-						//オープンリストに移してコストを計算する
-						open_list_.push_back(&m);
-						m.setParent(current, goal);
-						m.setListed(Mass::OPEN);
-					}
-				}
-			}
-		}
+		if (p.x() < goal.x()) { p.setX(p.x() + 1); continue; }
+		if (goal.x() < p.x()) { p.setX(p.x() - 1); continue; }
+		if (p.y() < goal.y()) { p.setY(p.y() + 1); continue; }
+		if (goal.y() < p.y()) { p.setY(p.y() - 1); continue; }
 	}
+
+	return false;
 }
 
 void Board::show() const 
