07fe9cbea4f6d8748b1bf034337c5722ff87a374
ZakiAKR <zakichan.ak@gmail.com>
2023-05-08 03:01:52 +0900

    授業を参考にコードを追記した。

--- src/PathFinding.cpp
+++ src/PathFinding.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-    Board board;
+    Board board;                     //ボードの宣言
 
-    Point* start = new Point(8, 8);
-    Point* goal = new Point(1, 1);
-    board.find(*start, *goal);
+    Point* start = new Point(8, 8);  //開始地点の設定。
+    Point* goal = new Point(1, 1);   //終了地点の設定。
+    board.find(*start, *goal);       //開始地点から終了地点のルートを見つける。
 
-    board.show();
+    board.show();                    //ボード上の効果や行動表示
 
-    system("PAUSE");
+    system("PAUSE");         //正常にコマンドが行われると０になる？
 }
