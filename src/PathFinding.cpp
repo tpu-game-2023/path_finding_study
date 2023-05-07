1139dc51505b7c4cefdded26d8ffff01bbd6f166
teru <74332407+Iketerumanato@users.noreply.github.com>
2023-05-07 17:20:44 +0900

    AddA*

--- src/PathFinding.cpp
+++ src/PathFinding.cpp
@@ -5,7 +5,9 @@ int main()
 {
     Board board;
 
+    //プレイヤー初期位置
     Point* start = new Point(8, 8);
+    //ゴール位置
     Point* goal = new Point(1, 1);
     board.find(*start, *goal);
 
--- src/board.cpp
+++ src/board.cpp
@@ -1,10 +1,70 @@
 #include "board.h"
+#include<algorithm>
+
+//ソート用の比較関数
+bool asc(const Mass* o1, const Mass* o2)
+{
+	return o1->getCost() < o2->getCost();
+}
 
 bool Board::find(const Point& start, const Point& goal)
 {
-	mass_[start.y()][start.x()].setStatus(Mass::START);
-	mass_[goal.y()][goal.x()].setStatus(Mass::GOAL);
+	//A*アルゴリズム
+	Mass& mass_start = getMass(start);
+	Mass& mass_goal = getMass(goal);
 
+	mass_start.setStatus(Mass::START);
+	mass_goal.setStatus(Mass::GOAL);
+
+	//オープンリストに開始ノードを追加
+	open_list_.clear();
+	open_list_.push_back(&mass_start);
+
+	while (!open_list_.empty())//オープンリストが空でない
+	{
+		//現在のノード=オープンリストの最も安価のリスト
+		std::sort(open_list_.begin(), open_list_.end(), asc);
+		auto it = open_list_.begin();
+		Mass* current = *it;
+		if (current->getStatus() == Mass::GOAL)
+		{
+			//ゴールなら経路を完成させる
+			Mass* p = current;//経路のステータスをMass::WayPointにする
+			while (p)
+			{
+				if (p->getStatus() == Mass::BLANK) p->setStatus(Mass::WAYPOINT);
+				p = p->getParent();
+			}
+			return true;
+		}
+		else
+		{
+			//現在のノードをクローズドリストに移す
+			open_list_.erase(it);
+			current->setListed(Mass::CLOSE);
+			//現在のノードと隣接するノードを調べる
+			const Point& pos = current->getPos();
+			Point next[4] = { pos.getRight(),pos.getLeft(),pos.getUp(),pos.getDown() };
+			for (auto& c : next)//隣接ノード
+			{
+				if (c.x() < 0 || BOARD_SIZE <= c.x())continue;//マップ外ならスキップ
+				if (c.y() < 0 || BOARD_SIZE <= c.y())continue;
+				Mass& m = getMass(c);
+				if (!m.isListed(Mass::OPEN) &&//オープンリストに含まれていない
+					!m.isListed(Mass::CLOSE) &&//クローズドリストに含まれていない
+					m.getStatus() != Mass::WALL) {//障害物でない
+					//オープンリストに移してコストを計算する
+					open_list_.push_back(&m);
+					m.setParent(current, goal);
+					m.setListed(Mass::OPEN);
+				}
+			}
+		}
+	}
+	return false;//到達しない
+
+	/*mass_[start.y()][start.x()].setStatus(Mass::START);
+	mass_[goal.y()][goal.x()].setStatus(Mass::GOAL);
 	Point p = start;
 	while (p != goal) {
 		if (p != start) mass_[p.x()][p.y()].setStatus(Mass::WAYPOINT);
@@ -14,8 +74,7 @@ bool Board::find(const Point& start, const Point& goal)
 		if (p.y() < goal.y()) { p.setY(p.y() + 1); continue; }
 		if (goal.y() < p.y()) { p.setY(p.y() - 1); continue; }
 	}
-
-	return false;
+	return false;*/
 }
 
 void Board::show() const 
@@ -64,5 +123,4 @@ void Board::show() const
 		std::cout << "+-";
 	}
 	std::cout << "+" << std::endl;
-
 }
